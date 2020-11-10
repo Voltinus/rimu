f80531997ba4725ad584db8f9d8e5876925e2f2c
Voltinus <voltinusmail@gmail.com>
2020-11-10 20:15:00 +0100

    Added death screen

--- templates/Level/GUI/GUI.gd
+++ templates/Level/GUI/GUI.gd
@@ -38,3 +38,17 @@ func update_player_stats(hp_left: float, hp_runes: int = 0, attack_runes: int = 
 
 func _on_Player_stats_changed(hp_left, hp_runes, attack_runes):
 	update_player_stats(hp_left, hp_runes, attack_runes)
+
+
+func _on_Player_died():
+	if not $GameOver.visible:
+		$GameOver.visible = true
+		$GameOver/AnimationPlayer.play('red_to_black')
+
+
+func _on_Retry_pressed():
+	get_tree().reload_current_scene()
+
+
+func _on_MainMenu_pressed():
+	Global.goto_scene('MainMenu')
